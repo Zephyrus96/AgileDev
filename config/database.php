c7cf24b2a7fa7fabcf59edc9df67bf0e9e446f31
Ali Bazzi <bazzi@bazman.dev>
2019-09-26 16:31:23 +0300

    deployment

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'agiledb'),
             'username' => env('DB_USERNAME', 'bazman'),
