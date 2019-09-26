4322cfba928df0654ae73cd481b47e56986a8eec
Ali Bazzi <bazzi@bazman.dev>
2019-09-26 17:28:49 +0300

    deployment

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,22 @@
 */
 
 //User authentication routes.
-Auth::routes();
+// Auth::routes();
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-Route::get('registers', 'Auth\RegisterController@showRegistrationForm');
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
 
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //Home and dashboard routes.
 Route::get('/', 'HomeController@index');
